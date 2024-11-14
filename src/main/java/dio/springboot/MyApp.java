1e874078939597c547ee371c50c1be69503ea650
Eduardo <eduardo25082002@gmail.com>
2024-11-14 13:58:35 -0300

    Primeiro Commit

---
+++ src/main/java/dio/springboot/MyApp.java
@@ -0,0 +1,15 @@
+package dio.springboot;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyApp implements CommandLineRunner {
+
+    private Calculadora calculadora;
+
+    @Override
+    public void run(String[] args) {
+        // TODO
+    }
+}
